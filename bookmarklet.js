5258ab5770f23684309f2e61ab934f21d0eda8b4
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-05-26 15:51:34 +0200

    Initial commit

---
+++ bookmarklet.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+	_my_script=document.createElement("SCRIPT");
+	_my_script.type="text/javascript";
+	_my_script.src="http://localhost:8080/parse.js";
+	document.getElementsByTagName("head")[0].appendChild(_my_script);
+})();
