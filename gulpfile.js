dfad6941c64060b0a952360746038be3143bfe88
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-05-26 17:52:57 +0200

    Added vendor deps and set up build env.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+
+var buildVendorScripts = function (dest) {
+	var sources = [
+		"bower_components/q/q.js",
+		"bower_components/ddp.js/ddp.js",
+		"bower_components/asteroid/dist/asteroid.js"
+	];
+	return gulp.src(sources)
+		.pipe(plugins.concat("vendor.js"))
+		.pipe(gulp.dest(dest));
+};
+
+gulp.task("build", function () {
+	buildVendorScripts(".");
+});
