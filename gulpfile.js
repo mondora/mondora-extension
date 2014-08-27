7e1e0597f52924664d28bdce6cea5985f529fb9f
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-08-27 10:16:43 +0200

    Updated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,68 +3,123 @@ var concat	= require("gulp-concat");
 var pp		= require("gulp-preprocess");
 var mkdirp	= require("mkdirp");
 var exec	= require("child_process").exec;
+var Q		= require("q");
 
 var buildVendorScripts = function (dest) {
-	var sources = [
+	console.log("Building vendor scripts");
+	var deferred = Q.defer();
+	gulp.src([
 		"bower_components/q/q.js",
-		"bower_components/ddp.js/ddp.js",
-		"bower_components/asteroid/dist/asteroid.js"
-	];
-	return gulp.src(sources)
+		"bower_components/ddp.js/src/ddp.js",
+		"bower_components/asteroid/dist/asteroid.chrome.js"
+	])
 		.pipe(concat("vendor.js"))
-		.pipe(gulp.dest(dest));
+		.pipe(gulp.dest(dest))
+		.on("end", function () {
+			deferred.resolve();
+		});
+	return deferred.promise;
 };
 
 var buildScripts = function (dest, target) {
-	return gulp.src("ext/js/*.js")
+	console.log("Building scripts");
+	var deferred = Q.defer();
+	gulp.src("ext/js/*.js")
 		.pipe(pp({
 			context: {
 				TARGET: target
 			}
 		}))
-		.pipe(gulp.dest(dest));
+		.pipe(gulp.dest(dest))
+		.on("end", function () {
+			deferred.resolve();
+		});
+	return deferred.promise;
 };
 
 var buildStyles = function (dest) {
-	return gulp.src("ext/css/*.css")
-		.pipe(gulp.dest(dest));
+	console.log("Building styles");
+	var deferred = Q.defer();
+	gulp.src("ext/css/*.css")
+		.pipe(gulp.dest(dest))
+		.on("end", function () {
+			deferred.resolve();
+		});
+	return deferred.promise;
 };
 
 var buildImages = function (dest) {
-	return gulp.src("ext/img/*")
-		.pipe(gulp.dest(dest));
+	console.log("Building images");
+	var deferred = Q.defer();
+	gulp.src("ext/img/*")
+		.pipe(gulp.dest(dest))
+		.on("end", function () {
+			deferred.resolve();
+		});
+	return deferred.promise;
 };
 
 var buildManifest = function (dest) {
-	return gulp.src("ext/manifest.json")
-		.pipe(gulp.dest(dest));
+	console.log("Building manifest");
+	var deferred = Q.defer();
+	gulp.src("ext/manifest.json")
+		.pipe(gulp.dest(dest))
+		.on("end", function () {
+			deferred.resolve();
+		});
+	return deferred.promise;
 };
 
 gulp.task("buildProd", function () {
-	mkdirp.sync("builds/prod/js");
-	mkdirp.sync("builds/prod/css");
-	mkdirp.sync("builds/prod/img");
-
-	buildVendorScripts("builds/prod/js/");
-	buildScripts("builds/prod/js/", "prod");
-	buildStyles("builds/prod/css/");
-	buildImages("builds/prod/img/");
-	buildManifest("builds/prod/");
-
-	exec("rm builds/mondora-extension.zip");
-	exec("cd builds/ && zip -r mondora-extension.zip prod/");
+	return Q()
+		.then(function () {
+			mkdirp.sync("builds/prod/js");
+			mkdirp.sync("builds/prod/css");
+			mkdirp.sync("builds/prod/img");
+		})
+		.then(function () {
+			return buildVendorScripts("builds/prod/js/");
+		})
+		.then(function () {
+			return buildScripts("builds/prod/js/", "prod");
+		})
+		.then(function () {
+			return buildStyles("builds/prod/css/");
+		})
+		.then(function () {
+			return buildImages("builds/prod/img/");
+		})
+		.then(function () {
+			return buildManifest("builds/prod/");
+		})
+		.then(function () {
+			exec("rm builds/mondora-extension.zip");
+			exec("cd builds/ && zip -r mondora-extension.zip prod/");
+		});
 });
 
 gulp.task("buildTest", function () {
-	mkdirp.sync("builds/test/js");
-	mkdirp.sync("builds/test/css");
-	mkdirp.sync("builds/test/img");
-
-	buildVendorScripts("builds/test/js/");
-	buildScripts("builds/test/js/", "test");
-	buildStyles("builds/test/css/");
-	buildImages("builds/test/img/");
-	buildManifest("builds/test/");
+	return Q()
+		.then(function () {
+			mkdirp.sync("builds/test/js");
+			mkdirp.sync("builds/test/css");
+			mkdirp.sync("builds/test/img");
+		})
+		.then(function () {
+			return buildVendorScripts("builds/test/js/");
+		})
+		.then(function () {
+			return buildScripts("builds/test/js/", "test");
+		})
+		.then(function () {
+			return buildStyles("builds/test/css/");
+		})
+		.then(function () {
+			return buildImages("builds/test/img/");
+		})
+		.then(function () {
+			return buildManifest("builds/test/");
+		});
 });
 
 gulp.task("default", ["buildTest", "buildProd"]);
